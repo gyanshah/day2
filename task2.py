2a1fc03b1f4bc3575b264c214fec4c02fee5b4c6
gyanshah <Gyan.shah@dal.ca>
2018-01-17 09:00:55 -0400

    1

--- task2.py
+++ task2.py
@@ -1,17 +1,20 @@
 def comparision(a,b,c,d):
 	if a>b:
 		if a>c:
+			comparision(a,b,c,d)
 			if a>d:
 				print("THe largest number is:",a)
 	elif b>a:
 		if b>c:
+			comparision(a,b,c,d)
 			if b>d:
 				print("The largest number is:",b)
 	elif c>a:
 		if c>b:
+			comparision(a,b,c,d)
 			if c>d:
 				print("The largest number is:",c)
 	else:
 		print ("The largest number is:",d)
 
-comparision(1,2,3,4)			
+comparision(1,2,3,2)
